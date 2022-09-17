17fd55dfc24e6fa4ab38797d7230a53b11402b45
efar716 <110000165+efar716@users.noreply.github.com>
2022-09-17 21:41:17 +1200

    repo update

--- music/adapters/memory_repository.py
+++ music/adapters/memory_repository.py
@@ -1,15 +1,27 @@
-
+import os
 from music.adapters.repository import AbstractRepository
 from music.adapters.csvdatareader import TrackCSVReader
+
 
 class MemoryRepository(AbstractRepository):
     # Articles ordered by date, not id. id is assumed unique.
 
     def __init__(self):
 
-        self.file_reader = TrackCSVReader("","")
-        self.tracks = list()
+        album_path = str(os.path.abspath("data/raw_albums_excerpt.csv"))
+        tracks_path = str(os.path.abspath("data/raw_tracks_excerpt.csv"))
+        self.file_reader = TrackCSVReader(album_path,tracks_path )
+        self.tracks = self.file_reader.read_csv_files()
 
-    def get_tracks(self, user_name):
-        return 0
+    def get_tracks_album(self,album_name):
+        tracks = []
+        for song in self.tracks:
+            if song.album is not None:
+                if type(song.album.title) == str and song.album.title == album_name:
+                    tracks.append(song)
+        return tracks
 
+
+
+trial = MemoryRepository()
+print(trial.get_tracks_album("Boss of Goth"))
--- music/adapters/repository.py
+++ music/adapters/repository.py
@@ -13,7 +13,7 @@ class AbstractRepository(abc.ABC):
 
 
     @abc.abstractmethod
-    def get_tracks(self, user_name):
+    def get_tracks_album(self, album_name):
         """ Returns the User named user_name from the repository.
 
         If there is no User with the given user_name, this method returns None.
