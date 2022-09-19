ec18181e462c684d08700b2fb6e6200937b18c93
awei409 <nomnomber@gmail.com>
2022-09-19 18:30:16 +1200

    Commenting out redundant code for test

--- music/utilities/utilities.py
+++ music/utilities/utilities.py
@@ -9,18 +9,18 @@ utilities_blueprint = Blueprint(
     'utilities_bp', __name__)
 
 
-def get_tags_and_urls():
-    tag_names = services.get_tag_names(repo.repo_instance)
-    tag_urls = dict()
-    for tag_name in tag_names:
-        tag_urls[tag_name] = url_for('news_bp.articles_by_tag', tag=tag_name)
+# def get_tags_and_urls():
+#     tag_names = services.get_tag_names(repo.repo_instance)
+#     tag_urls = dict()
+#     for tag_name in tag_names:
+#         tag_urls[tag_name] = url_for('news_bp.articles_by_tag', tag=tag_name)
 
-    return tag_urls
+#     return tag_urls
 
 
-def get_selected_articles(quantity=3):
-    articles = services.get_random_articles(quantity, repo.repo_instance)
+# def get_selected_articles(quantity=3):
+#     articles = services.get_random_articles(quantity, repo.repo_instance)
 
-    for article in articles:
-        article['hyperlink'] = url_for('news_bp.articles_by_date', date=article['date'].isoformat())
-    return articles
+#     for article in articles:
+#         article['hyperlink'] = url_for('news_bp.articles_by_date', date=article['date'].isoformat())
+#     return articles
