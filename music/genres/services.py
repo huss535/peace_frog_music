86b3ba7a0e6edec548bdf2c0e77487c1b8bf7137
awei409 <nomnomber@gmail.com>
2022-09-23 20:56:42 +1200

    UnknownUserExcepton in services.py(art,gen,trklst)

--- music/genres/services.py
+++ music/genres/services.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from music.adapters.repository import AbstractRepository
 from music.adapters.memory_repository import MemoryRepository
 
+class UnknownUserException(Exception):
+    pass
 
 def retrieve_tracks(repo: AbstractRepository, genre_name):
 
