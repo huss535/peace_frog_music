e7ad872edefb259f07a017891848b63b72d8d8e7
efar716 <110000165+efar716@users.noreply.github.com>
2022-10-08 17:55:09 +1300

    mapper

--- music/adapters/orm.py
+++ music/adapters/orm.py
@@ -4,44 +4,14 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import mapper, relationship, synonym
 
-
 # global variable giving access to the MetaData (schema) information of the database
-from music.domainmodel import track, artist
+from music import Track
+from music.domainmodel import track, artist, album
+from music.domainmodel.album import Album
+from music.domainmodel.artist import Artist
+from music.domainmodel.genre import Genre
 
 metadata = MetaData()
-
-tracks_table = Table(
-    'tracks', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('track_id', Integer, unique=True, nullable=False),
-    Column('artists_id',  ForeignKey('artists.artist_id')),
-    Column('title', String(255), nullable=False),
-    Column('track_url', String(255), nullable=False),
-    Column('track_duration', Integer, nullable=False)
-)
-
-artists_table = Table(
-    'artists', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('artist_id', Integer, unique=True, nullable=False),
-    Column('full_name', String(1024), nullable=False)
-
-)
-
-users_table = Table(
-    'users', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('user_id', Integer, unique=True, nullable=False),
-    Column('user_name', String(1024), unique=True, nullable=False),
-    Column('password', String(1024), nullable=False)
-)
-
-genres_table = Table(
-    'genres', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('genre_id', Integer, unique=True, nullable=False),
-    Column('name', String(1024), unique=True, nullable=False)
-)
 
 albums_table = Table(
     'albums', metadata,
@@ -53,14 +23,70 @@ albums_table = Table(
     Column('release_year', Integer)
 )
 
+artists_table = Table(
+    'artists', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('artist_id', Integer, unique=True, nullable=False),
+    Column('full_name', String(1024), nullable=False)
+
+)
+
+genres_table = Table(
+    'genres', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('genre_id', Integer, unique=True, nullable=False),
+    Column('name', String(1024), unique=True, nullable=False)
+)
+
+tracks_table = Table(
+    'tracks', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('track_id', Integer, unique=True, nullable=False),
+    Column('artists_id', ForeignKey('artists.artist_id')),
+    Column('title', String(255), nullable=False),
+    Column('track_url', String(255), nullable=False),
+    Column('track_duration', Integer, nullable=False)
+)
+
+users_table = Table(
+    'users', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('user_id', Integer, unique=True, nullable=False),
+    Column('user_name', String(1024), unique=True, nullable=False),
+    Column('password', String(1024), nullable=False)
+)
+
 
 def map_model_to_tables():
-    mapper(track, tracks_table, properties={
-        '_track__title': tracks_table.c.title,
-        '_track__tracks_id': tracks_table.c.track_id,
-        '_track__track_url': tracks_table.c.track_url,
-        '_track__track_duration': tracks_table.c.track_duration,
-        '_track__artist': relationship(artist, backref="_artist__artist_id")
+    mapper(Album, albums_table, properties={
+        '_Album__title': albums_table.c.title,
+        '_Album__album_id': albums_table.c.album_id,
+        '_Album__album_url': albums_table.c.album_url,
+        '_Album__album_type': albums_table.c.album_type,
+        '_Album__release_year': albums_table.c.release_year
 
     })
 
+    mapper(Artist, artists_table, properties={
+        '_Artist__full_name': artists_table.c.full_name,
+        '_Artist__artist_id': artists_table.c.artist_id,
+
+    })
+
+    mapper(Genre, genres_table, properties={
+        '_Genre__genre_id': genres_table.c.genre_id,
+        '_Genre__name': genres_table.c.name,
+
+    })
+
+    mapper(Track, tracks_table, properties={
+        '_Track__title': tracks_table.c.title,
+        '_Track__track_id': tracks_table.c.track_id,
+        '_Track__track_url': tracks_table.c.track_url,
+        '_Track__track_duration': tracks_table.c.track_duration,
+        '_Track__album': relationship(artist, backref="_artist__artist_id"),
+        '_Track__artist': relationship(artist, backref="_artist__artist_id"),
+        '_Track__genres': relationship(artist, backref="_artist__artist_id")
+
+
+    })
--- music/adapters/repository.py
+++ music/adapters/repository.py
@@ -14,14 +14,14 @@ class AbstractRepository(abc.ABC):
     def add_tracks(self, alb):
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_tracks_album(self, album_name):
-        raise NotImplementedError
+   # @abc.abstractmethod
+   # def get_tracks_album(self, album_name):
+       # raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_tracks_genre(self, album_name):
-        raise NotImplementedError
+   # @abc.abstractmethod
+   # def get_tracks_genre(self, album_name):
+       # raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_tracks_artist(self, album_name):
-        raise NotImplementedError
+   # @abc.abstractmethod
+  #  def get_tracks_artist(self, album_name):
+      #  raise NotImplementedError
